95738112b98ff6aacdf97683cc49ba64a406f1c7
root <root@ip-172-31-11-80.ap-south-1.compute.internal>
2024-09-05 18:18:54 +0000

    minikube installation commands

---
+++ minikube.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install -y docker.io
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+sudo mv minikube /usr/local/bin/
+sudo minikube start --vm-driver=docker --force
+minikube status
+
